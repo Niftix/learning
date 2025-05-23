39b40b7ec3652fd45e5fe16a8326ed88739b14bf
Niftix <Mathdigitale@gmail.com>
2025-05-23 21:11:42 +0000

    update

---
+++ week.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+
+int taille_bit(int n);
+int binaire(int *tab, int entier, int taille);
+void affichage(int *tableau);
+
+int main()
+{
+	int size = taille_bit(89);
+	int *taille = malloc(sizeof(int) * size);
+	if (taille == NULL)
+	{
+		printf("erreur\n");
+		return 1;
+	}
+	binaire(taille, 89, size);
+	affichage(taille);
+	return 0;
+}
+
+int taille_bit(int n)
+{
+	int taille = 0;
+	int temp = n;
+	while (temp > 0)
+	{
+	taille++;
+	temp = temp >> 1;
+	}
+	return taille;
+}
+
+int binaire(int *tab, int entier, int taille)
+{
+	int i = 0;
+	int temp = entier;
+	while (temp > 0)
+	{
+		if ((temp & 1) == 0)
+		{
+			tab[i] = 0;
+		}
+		if ((temp & 1) == 1)
+		{
+			tab[i] = 1;
+		}
+		temp = temp >> 1;
+		i++;
+	}
+	tab[taille + 1] = '\0';
+return *tab;
+}
+
+void affichage(int *tableau)
+{
+	int i = 0;
+	while(tableau[i] != '\0')
+	{
+		printf("%d", tableau[i]);
+		i++;
+	}
+}
