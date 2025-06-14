a117ddc8548eed2de700904bb64a99fcb9b6254a
Niftix <Mathdigitale@gmail.com>
2025-06-14 17:00:10 +0000

    nice

--- ft_putnbr.c
+++ ft_putnbr.c
@@ -1,36 +1,29 @@
 #include <unistd.h>
+#include <stdio.h>
 
 void ft_putnbr(int n)
 {
-	int	save;
-	int	i;
-	char	printer;
+	char	temp;
 
-	save = 0;
-	i = 0;
-	while(n > 10)
+	temp = '0';	
+        if (n % 10 > 0)
+        {
+                temp += n % 10;
+        }
+	if (n < 10)
 	{
-		save += (n % 10);
-		n /= 10;
-		save *= 10;	
-		i++;
+		write (1, &temp, 1);
+		return;
 	}
-	save += (n % 10);
-	i++;
-	while (i > 0)
-	{
-		printer = (save % 10) + '0';
-		write (1, &printer, 1);
-		save /= 10;
-		i--;
-	}
+	ft_putnbr(n/10);
+        write (1, &temp, 1);
+
 }
-/*
+
 int main ()
 {
 	int	nb;
-
 	scanf("%d", &nb);	
 	ft_putnbr(nb);
 	return 0;
-}*/
+}
