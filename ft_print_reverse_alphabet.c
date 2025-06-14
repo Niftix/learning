301d5a939703b483b9c9b7e2c0ba8ab4deabcd72
Niftix <Mathdigitale@gmail.com>
2025-06-14 12:17:29 +0000

    exo3

---
+++ ft_print_reverse_alphabet.c
@@ -0,0 +1,20 @@
+#include <unistd.h>
+
+void ft_print_reverse_alphabet(void)
+{
+	char start;
+	int	i;
+
+	start = 'z';
+	for (i = 0; i < 26; i++)
+	{
+		write (1, &start, 1);
+		start--;
+	}
+}
+
+int main (void)
+{
+	ft_print_reverse_alphabet();
+	return 0;
+}
