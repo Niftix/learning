aea53cf632f1e138fc2f16de1e7e7c3dbf1da2d5
Niftix <Mathdigitale@gmail.com>
2025-06-14 12:31:49 +0000

    update

--- ft_is_negative.c
+++ ft_is_negative.c
@@ -4,11 +4,15 @@ void ft_is_negative(int n)
 {
 	if (n > 0)
 	{
-	write (1, "P", 1);
+		write (1, "P", 1);
 	}
 	else if (n < 0)
 	{
-	write (1, "N", 1);
+		write (1, "N", 1);
+	}
+	else if (n == 0)
+	{
+		write (1, "P", 1 );
 	}
 }
 /*
